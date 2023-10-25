c174cc2a5395ec057d947e5edb7a074abd69106f
Alejandro Mejía <alejomejiacuartas@outlook.com>
2023-10-25 17:50:12 -0500

    Remove not neccessary hooks

--- src/hooks/utils/getEntityId.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Using the entity `url`, returns the entity `id` as `number`
- */
-
-export function getEntityId(url: string): number {
-  const parts = url.split('/')
-  return Number(parts[parts.length - 2])
-}

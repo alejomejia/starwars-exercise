fb9162e4c6ba26bbb592a59bdd5fe6377a18c6bb
Alejandro Mejía <alejomejiacuartas@outlook.com>
2023-10-26 14:29:46 -0500

    Update CountView component logic

--- src/components/CountView/CountView.tsx
+++ src/components/CountView/CountView.tsx
@@ -15,13 +15,18 @@ export function CountView({ id, entity }: Props) {
   const [_, setViews] = useLocalStorage<LocalStorageViews>('views', LS_INITIAL_VIEWS)
 
   useEffect(() => {
-    setViews((prev) => ({
-      ...prev,
-      [entity]: {
-        ...prev[entity],
-        [id]: (prev[entity]?.[id] ?? 0) + 1
+    setViews((prev) => {
+      const currentIdCount = prev[entity]?.[id]
+      const idCount = (currentIdCount ?? 0) + 1
+
+      return {
+        ...prev,
+        [entity]: {
+          ...prev[entity],
+          [id]: idCount
+        }
       }
-    }))
+    })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id])
 

7db99d744b59ba3bac97fb2ed9fa40b8dc092ac3
Alejandro Mejía <alejomejiacuartas@outlook.com>
2023-10-27 16:06:11 -0500

    Add animations to PageDetails

--- src/components/PageDetails/PageDetails.tsx
+++ src/components/PageDetails/PageDetails.tsx
@@ -1,3 +1,9 @@
+'use client'
+
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import { variantImage, variantContent } from './animation'
+
 type Title = {
   main: string
   secondary?: string
@@ -21,19 +27,25 @@ export function PageDetails({ title, imageUrl, description, details }: Props) {
 
   return (
     <div className="flex gap-8 text-white">
-      <picture className="w-full max-w-sm">
-        <img
+      <motion.picture className="w-full max-w-sm" variants={variantImage} initial="initial" animate="animate">
+        <Image
           className="object-cover object-top w-full h-96 rounded-xl"
           src={imageUrl ?? '/images/resource-fallback.jpg'}
           alt={title.main}
+          width={400}
+          height={400}
         />
-      </picture>
-      <div className="flex-1">
-        {title.secondary && <h1 className="mb-2 text-xl font-medium text-slate-300">{title.secondary}</h1>}
-        <Heading className="inline-block text-4xl font-bold mb-8 border-b-4 border-b-yellow-300">{title.main}</Heading>
-        {description && <p className="text-lg mb-8">{description}</p>}
-        <ul>{detailsInfo}</ul>
-      </div>
+      </motion.picture>
+      <motion.div variants={variantContent} initial="initial" animate="animate">
+        <div className="flex-1">
+          {title.secondary && <h1 className="mb-2 text-xl font-medium text-slate-300">{title.secondary}</h1>}
+          <Heading className="inline-block text-4xl font-bold mb-8 border-b-4 border-b-yellow-300">
+            {title.main}
+          </Heading>
+          {description && <p className="text-lg mb-8">{description}</p>}
+          <ul>{detailsInfo}</ul>
+        </div>
+      </motion.div>
     </div>
   )
 }
---
+++ src/components/PageDetails/animation.ts
@@ -0,0 +1,27 @@
+export const variantImage = {
+  initial: {
+    opacity: 0,
+    x: -24
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: 0.1
+    }
+  }
+}
+
+export const variantContent = {
+  initial: {
+    opacity: 0,
+    x: 24
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: 0.2
+    }
+  }
+}
